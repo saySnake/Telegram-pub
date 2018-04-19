b7ef9a0ad183cc95818e33fa47b05d67780b2e6c
saySnake <583151161@qq.com>
2018-04-19 19:52:55 +0800

    update

--- Telegraph/RedPackVC/RedContentVC.m
+++ Telegraph/RedPackVC/RedContentVC.m
@@ -6,8 +6,18 @@
 //
 
 #import "RedContentVC.h"
+#import "DDButton.h"
 
 
+@interface RedContentVC ()
+
+@property (nonatomic ,strong)DDButton *leftBtn;
+
+@property (nonatomic ,strong)UIImageView *titleImg;
+
+@property (nonatomic ,strong)UILabel *nameTitleLabel;
+
+@property (nonatomic ,strong)UILabel *contenLabel;
 
 
 @interface RedContentVC ()
@@ -19,12 +29,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.navBar setHidden:YES];
-
+    UIView *viewS =self.navigationController.navigationBar;
+    [viewS setHidden:YES];
+    
+    UIView *view =[[UIView alloc] initWithFrame:CGRectMake(0, 0, DScreenW, 64)];
+    view.backgroundColor =[UIColor redColor];
+    [self.view addSubview:view];
+    
+    self.leftBtn=[[DDButton alloc] initWithImageFrame:CGRectMake(12, 12, 20, 20)];
+    self.leftBtn.frame=CGRectMake(0, 20, 60, 44);
+    self.leftBtn.tag=0;
+    self.leftBtn.hidden=YES;
+    [self.leftBtn addTarget:self action:@selector(actionButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.leftBtn];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+
+- (void)actionButton:(UIButton *)sender
+{
+    NSLog(@"返回");
+    [self.navigationController popViewControllerAnimated:YES];
+    
 }
 
 
+- (void)setUpViews
+{
+    
+}
 @end
